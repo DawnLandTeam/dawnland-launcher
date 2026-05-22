e1dd17719100bbdb51675c767d53eacd8bdd10a2
MaoZa <cap_sub@dawnland.cn>
2026-05-22 21:17:13 +0800

    feat: 完成 Phase 2 里程碑功能更新

---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import InstancesView from "../views/InstancesView.vue";
+import DownloadsView from "../views/DownloadsView.vue";
+import SettingsView from "../views/SettingsView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/instances",
+      name: "instances",
+      component: InstancesView,
+    },
+    {
+      path: "/downloads",
+      name: "downloads",
+      component: DownloadsView,
+    },
+    {
+      path: "/settings",
+      name: "settings",
+      component: SettingsView,
+    },
+  ],
+});
+
+export default router;
